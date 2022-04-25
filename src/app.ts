aae45b11f0e9c5368226ed0144f8fd5d0e92cee2
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-04-26 01:16:08 +0500

    update

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,51 @@
-import { Bot } from "grammy";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Create an instance of the `Bot` class and pass your authentication token to it.
-const bot = new Bot(process.env.TOKEN || ""); // <-- put your authentication token between the ""
+import { Bot, Context, Keyboard, session, SessionFlavor } from "grammy";
 
-// You can now register listeners on your bot object `bot`.
-// grammY will call the listeners when users send messages to your bot.
+// Define the shape of our session.
+interface SessionData {
+  pizzaCount: number;
+}
 
-// Handle the /start command.
-bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-// Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message !"));
+// Flavor the context type to include sessions.
+type MyContext = Context & SessionFlavor<SessionData>;
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
+const bot = new Bot<MyContext>(process.env.TOKEN || "");
 
-// Start the bot.
+// Install session middleware, and define the initial session value.
+function initial(): SessionData {
+  return { pizzaCount: 0 };
+}
+bot.use(session({ initial }));
+
+const keyboard = new Keyboard().text("I am Hungry: 🍕");
+
+bot.command("hunger", async (ctx) => {
+  const count = ctx.session.pizzaCount;
+  await ctx.reply(`Your hunger level is ${count}!`, {
+    reply_markup: {
+      resize_keyboard: true,
+      keyboard: keyboard.build(),
+    },
+  });
+});
+
+bot.command("remove", async (ctx) => {
+  await ctx.reply("Keyboard removed", {
+    reply_markup: { remove_keyboard: true },
+  });
+});
+
+bot.hears(/.*🍕.*/, async (ctx) => {
+  ctx.session.pizzaCount++;
+  const count = ctx.session.pizzaCount;
+  await ctx.reply(`Your hunger level is ${count}!`, {
+    reply_markup: {
+      resize_keyboard: true,
+      keyboard: keyboard.build(),
+    },
+  });
+});
+
 bot.start();
