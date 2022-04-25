aa7609f25682ec018e66c54c56052c87be74161f
Mukhammadyusuf Abdurakhimov <icoderx@yandex.com>
2022-04-25 23:28:51 +0500

    tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import { Bot } from "grammy";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Create an instance of the `Bot` class and pass your authentication token to it.
-const bot = new Bot(""); // <-- put your authentication token between the ""
+const bot = new Bot(process.env.TOKEN || ""); // <-- put your authentication token between the ""
 
 // You can now register listeners on your bot object `bot`.
 // grammY will call the listeners when users send messages to your bot.
@@ -9,7 +11,7 @@ const bot = new Bot(""); // <-- put your authentication token between the ""
 // Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 // Handle other messages.
-bot.on("message", (ctx) => ctx.reply("Got another message!"));
+bot.on("message", (ctx) => ctx.reply("Got another message !"));
 
 // Now that you specified how to handle messages, you can start your bot.
 // This will connect to the Telegram servers and wait for messages.
